1defa41e03ecbc97d4007dbe538c7d465dd52e7b
Joey Manso <jmanso522@gmail.com>
2022-12-03 10:11:46 -0600

    Completion of Day 3

--- dist/Days/Day03.js
+++ dist/Days/Day03.js
@@ -4,10 +4,37 @@ exports.Day03 = void 0;
 const Day_1 = require("./Day");
 class Day03 extends Day_1.Day {
     Run(_part) {
+        let prioritySum = 0;
         if (_part === Day_1.Part.Part01) {
+            for (const rucksack of this.inputArray) {
+                const midpoint = rucksack.length / 2;
+                const compartment1 = rucksack.substring(0, midpoint);
+                const compartment2 = rucksack.substring(midpoint, rucksack.length);
+                for (const char of compartment1) {
+                    if (compartment2.includes(char)) {
+                        prioritySum += this.GetCharValue(char);
+                        break;
+                    }
+                }
+            }
         }
         else if (_part === Day_1.Part.Part02) {
+            for (let i = 0; i < this.inputArray.length; i += 3) {
+                for (const char of this.inputArray[i]) {
+                    if (this.inputArray[i + 1].includes(char) && this.inputArray[i + 2].includes(char)) {
+                        prioritySum += this.GetCharValue(char);
+                        break;
+                    }
+                }
+            }
         }
+        console.log(`Sum of priorities: ${prioritySum}`);
+    }
+    GetCharValue(_char) {
+        if (_char.toUpperCase() == _char) {
+            return 27 + (_char.charCodeAt(0) - 'A'.charCodeAt(0));
+        }
+        return 1 + (_char.charCodeAt(0) - 'a'.charCodeAt(0));
     }
 }
 exports.Day03 = Day03;
--- src/Days/Day03.ts
+++ src/Days/Day03.ts
@@ -2,9 +2,37 @@ import {Day, Part} from "./Day";
 
 export class Day03 extends Day {
     public Run(_part: Part) {
+        let prioritySum = 0;
         if (_part === Part.Part01) {
+            for (const rucksack of this.inputArray) {
+                const midpoint: number = rucksack.length / 2;
+                const compartment1: string = rucksack.substring(0, midpoint);
+                const compartment2: string = rucksack.substring(midpoint, rucksack.length);
+                for (const char of compartment1) {
+                    if (compartment2.includes(char)) {
+                        prioritySum += this.GetCharValue(char);
+                        break;
+                    }
+                }
+            }
         }
         else if (_part === Part.Part02) {
+            for (let i = 0; i < this.inputArray.length; i+=3) {
+                for (const char of this.inputArray[i]) {
+                    if (this.inputArray[i+1].includes(char) && this.inputArray[i+2].includes(char)) {
+                        prioritySum += this.GetCharValue(char);
+                        break;
+                    }
+                }
+            }
         }
+        console.log(`Sum of priorities: ${prioritySum}`);
+    }
+
+    private GetCharValue(_char: string): number {
+        if (_char.toUpperCase() == _char) {
+            return 27 + (_char.charCodeAt(0) - 'A'.charCodeAt(0))
+        }
+        return 1 + (_char.charCodeAt(0) - 'a'.charCodeAt(0));
     }
 }

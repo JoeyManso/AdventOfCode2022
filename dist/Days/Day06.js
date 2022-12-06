32b0c8fa1b0cce913cf3c096831f69f0b43785c5
Joey Manso <jmanso522@gmail.com>
2022-12-06 16:06:21 -0600

    Completion of Day 6

--- dist/Days/Day06.js
+++ dist/Days/Day06.js
@@ -4,9 +4,17 @@ exports.Day06 = void 0;
 const Day_1 = require("./Day");
 class Day06 extends Day_1.Day {
     Run(_part) {
-        if (_part === Day_1.Part.Part01) {
-        }
-        else if (_part === Day_1.Part.Part02) {
+        const uniqueMarkerCharacters = _part === Day_1.Part.Part01 ? 4 : 14;
+        const input = this.inputArray[0];
+        for (let i = uniqueMarkerCharacters; i < input.length; ++i) {
+            const uniqueChars = new Set();
+            for (let j = i - uniqueMarkerCharacters; j < i; ++j) {
+                uniqueChars.add(input[j]);
+            }
+            if (uniqueChars.size == uniqueMarkerCharacters) {
+                console.log(`Starting Marker Detected at Index ${i}`);
+                break;
+            }
         }
     }
 }
--- src/Days/Day06.ts
+++ src/Days/Day06.ts
@@ -2,9 +2,17 @@ import {Day, Part} from "./Day";
 
 export class Day06 extends Day {
     public Run(_part: Part) {
-        if (_part === Part.Part01) {
-        }
-        else if (_part === Part.Part02) {
+        const uniqueMarkerCharacters = _part === Part.Part01 ? 4 : 14;
+        const input: string = this.inputArray[0];
+        for (let i = uniqueMarkerCharacters; i < input.length; ++i) {
+            const uniqueChars: Set<string> = new Set<string>();
+            for (let j = i - uniqueMarkerCharacters; j < i; ++j) {
+                uniqueChars.add(input[j]);
+            }
+            if (uniqueChars.size == uniqueMarkerCharacters) {
+                console.log(`Starting Marker Detected at Index ${i}`);
+                break;
+            }
         }
     }
 }
